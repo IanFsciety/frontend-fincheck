a6c29f54dc03bd6213331432353c58f3e3260b20
IanFsciety <iandelas123@outlook.com>
2024-08-25 16:40:13 -0400

    add create bankAccount

--- src/app/services/bankAccountsService/create.ts
+++ src/app/services/bankAccountsService/create.ts
@@ -1,18 +1,14 @@
 import { httpClient } from "../httpClient";
 
-
-export interface BankAccountParams {
-    name: string,
-    initialBalance: number,
-    color: string,
-    type: 'CHECKING' | 'INVESTMENT' | 'CASH'
+export interface CreateBankAccountParams {
+  name: string;
+	initialBalance: number;
+	color: string;
+	type: 'CHECKING' | 'INVESTMENT' | 'CASH';
 }
 
-
-export async function create(params: BankAccountParams) {
+export async function create(params: CreateBankAccountParams) {
   const { data } = await httpClient.post('/bank-accounts', params);
 
   return data;
 }
-
-
